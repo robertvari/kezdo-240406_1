c41ddcb0c619c5601816a1a933fc7c8688c26ac0
Robert Vari <mail.robertvari@gmail.com>
2024-04-06 17:07:13 +0200

    update

--- 15_conditions.py
+++ 15_conditions.py
@@ -1,19 +1,25 @@
-number = 11
+number = 13
 
+#    False
 if number == 10:
     print("The number is 10")
 
+#    True
 if number != 10:
     print("Number is not 10")
 
+#    False
 if number < 10:
     print("Number is less than 10")
 
+#   True
 if number > 10:
     print("Number is greater than 10")
 
+#   False
 if number <= 10:
     print("Number is less or equal to 10")
 
+#   True
 if number >= 10:
     print("Number is greater or equal to 10")
