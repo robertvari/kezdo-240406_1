2cce6be68c296aa591784b07d5feb8b434242e34
Robert Vari <mail.robertvari@gmail.com>
2024-04-06 16:05:01 +0200

    update

---
+++ 10_list_slicing.py
@@ -0,0 +1,7 @@
+my_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# list_name[START_INDEX:STOP_INDEX:STEP]
+print(my_numbers)
+print(my_numbers[-4:])
+print(my_numbers[::4])
+print(my_numbers[2:-2:2])
