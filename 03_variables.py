4da4ad039248ac0441fa24eb9b1faed417859343
Robert Vari <mail.robertvari@gmail.com>
2024-04-06 15:06:36 +0200

    update

---
+++ 03_variables.py
@@ -0,0 +1,7 @@
+name = input("What is your name?")
+address = input("Where do you live?")
+age = input("How old are you?")
+
+
+# f = formated string
+print(f"Hello {name}! You live in {address}. You are {age} years old.")
