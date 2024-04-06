55f3eea1535361e8b1d405e0418dc8701577b196
Robert Vari <mail.robertvari@gmail.com>
2024-04-06 15:42:46 +0200

    update

---
+++ 06_list_methods_append.py
@@ -0,0 +1,13 @@
+my_friends = []
+print(my_friends)
+
+name_1 = input("Your first friend's name?")
+my_friends.append(name_1)
+
+name_2 = input("Your second friend's name?")
+my_friends.append(name_2)
+
+name_3 = input("Your third friend's name?")
+my_friends.append(name_3)
+
+print(my_friends)
