d2bdbbe0a1b484e1c55e800ded82a724a365fb9a
Robert Vari <mail.robertvari@gmail.com>
2024-04-06 14:28:39 +0200

    Update

---
+++ 01_datatypes.py
@@ -0,0 +1,13 @@
+# string (str)
+"Hello my name is Vári Róbert."
+'Hello my name is Kiss Csaba.'
+"It's my birthday!!"
+
+# boolean (bool)
+True, False
+
+# integer
+1, 2, 3, -1, -2, -3, 12345, -123456
+
+# float
+3.14, -24.567
