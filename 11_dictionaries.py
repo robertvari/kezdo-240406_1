543bc6550fe3a9242411d57b86520d701ef593cf
Robert Vari <mail.robertvari@gmail.com>
2024-04-06 16:39:26 +0200

    update

---
+++ 11_dictionaries.py
@@ -0,0 +1,7 @@
+phonebook = {
+    "06201112233": { "name" : "Robert", "address": "Budapest", "email": "robert@gmail.com"},
+    "06202223344": { "name" : "Csaba", "address": "Pécs", "email": "csaba@gmail.com"},
+    "06203334455": { "name" : "Kriszta", "address": "Eger", "email": "kriszta@gmail.com"}
+}
+
+print(phonebook["06202223344"]["email"])
