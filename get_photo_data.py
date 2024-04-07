0fbbde885b03fa3572dd06f863279c4cda5bb0de
Robert Vari <mail.robertvari@gmail.com>
2024-04-07 16:49:26 +0200

    update

--- get_photo_data.py
+++ get_photo_data.py
@@ -1,4 +1,14 @@
-# todo store the full path to the photo directory
+import os
+
+# store the full path to the photo directory
+# r= raw string
+dir_path = r"D:\Work\_PythonSuli\kezdo-240406\alapok_1\photos"
+
+# todo chech path validity
+assert os.path.exists(dir_path), f"Folder does not exist: {dir_path}"
+
+# todo check if path is a directory
+assert os.path.isdir(dir_path), f"Path must be a directory: {dir_path}"
 
 
 
