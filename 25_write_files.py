36062ba4e1c08fbd1baab75515e1cd690d9eb9af
Robert Vari <mail.robertvari@gmail.com>
2024-04-07 15:51:22 +0200

    update

--- 25_write_files.py
+++ 25_write_files.py
@@ -3,5 +3,7 @@ address = input("Where do you live?")
 age = input("How old are you?")
 
 file_name = "user_data.txt"
-with open(file_name, "w") as f:
-    f.write(f"Hello {name}. You live in {address}. You are {age} years old.")
+
+# with context
+with open(file_name, "w") as my_file:
+    my_file.write(f"Hello {name}. You live in {address}. You are {age} years old.")
