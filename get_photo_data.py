0b4f1e55180f2427c905e464d647d291e3fa1ec3
Robert Vari <mail.robertvari@gmail.com>
2024-04-07 16:31:08 +0200

    update

---
+++ get_photo_data.py
@@ -0,0 +1,7 @@
+# todo store the full path to the photo directory
+
+
+
+# todo collect all images (.jpg/.jpeg) from the directory
+# todo collect data from files: file_name, file_size, pixel_size (width, height)
+# todo write out data in json format
