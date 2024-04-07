3ed1ff2f10c6dbbaa252a1fa7543c3566b7c880b
Robert Vari <mail.robertvari@gmail.com>
2024-04-07 17:49:57 +0200

    update

--- get_photo_data.py
+++ get_photo_data.py
@@ -1,4 +1,6 @@
-import os
+import os, json
+from PIL import Image
+
 
 # store the full path to the photo directory
 # r= raw string
@@ -10,12 +12,27 @@ assert os.path.exists(dir_path), f"Folder does not exist: {dir_path}"
 # check if path is a directory
 assert os.path.isdir(dir_path), f"Path must be a directory: {dir_path}"
 
-# todo collect all images (.jpg/.jpeg) from the directory
+# collect all images (.jpg) from the directory
 folder_content = os.listdir(dir_path)
 print(f"File count: {len(folder_content)}")
 
+photo_files = []
 for i in folder_content:
-    print(i)
+    if not i.endswith(".jpg") and not i.endswith(".jpeg"):
+        continue
 
-# todo collect data from files: file_name, file_size, pixel_size (width, height)
+    photo_files.append(i)
+
+
+# collect data from files: file_name, file_size, pixel_size (width, height)
+photo_data = {}
+for i in photo_files:
+    file_name = os.path.join(dir_path, i)
+    file_size = os.path.getsize(file_name) / (1024*1024)
+
+    img = Image.open(file_name)
+    pixel_size = img.size
+
+    photo_data[file_name] = {"file_size": file_size, "pixel_size": pixel_size}
+
 # todo write out data in json format
