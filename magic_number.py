d8c7c2bddfa661175b7cc6fe1002a2675974d572
Robert Vari <mail.robertvari@gmail.com>
2024-04-07 14:47:46 +0200

    update

--- magic_number.py
+++ magic_number.py
@@ -5,6 +5,16 @@ magic_number = random.randint(1, 10)
 # cast a string to an int
 player_number = input("Guess my number:")
 
+
+
+while str(magic_number) != player_number:
+    print("Wrong guess! Try again.")
+    player_number = input("Guess my number:")
+
+
+
+
+
 if str(magic_number) == player_number:
     print(f"You win! {magic_number} was my number :)")
 else:
